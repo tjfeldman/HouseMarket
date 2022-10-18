f6c6121896836d7630350d2506f436d038540450
Feldman <625711@bah.com>
2022-10-18 08:17:23 -0400

    Initial Commit

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRxGff3Ejc9u_w5aXUb5WseXUyWjs6ZYs",
+  authDomain: "house-marketplace-app-7dd09.firebaseapp.com",
+  projectId: "house-marketplace-app-7dd09",
+  storageBucket: "house-marketplace-app-7dd09.appspot.com",
+  messagingSenderId: "297685143207",
+  appId: "1:297685143207:web:c661fdeff837b965ba1582",
+  measurementId: "G-19JY3RZ3PW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore();
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
+import { updateDoc, doc } from 'firebase/firestore'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
+
+function Profile() {
+    const auth = getAuth()
+    const [changeDetails, setChangeDetails] = useState(false)
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+    })
+
+    const {name, email} = formData
+
+    const navigate = useNavigate()
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }))
+    }
+
+    const onSubmit = async (e) => {
+        try {
+            if (auth.currentUser !== name) {
+                //Update display name in firebase
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+                })
+
+                //update in firestore
+                const userRef = doc(db, 'users', auth.currentUser.uid)
+                await updateDoc(userRef, {
+                    name
+                })
+            }
+
+        } catch (error) {
+            toast.error('Could not update profile details')
+        }
+    }
+
+    const onLogout = (e) => {
+        auth.signOut()
+        navigate('/')
+    }
+
+    return <div className="profile">
+        <header className="profileHeader">
+            <p className="pageHeader">My Profile</p>
+            <button type="button" className="logOut" onClick={onLogout}>Logout</button>
+        </header>
+
+        <main>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">Personal Details</p>
+                <p className="changePersonalDetails" onClick={() => {
+                    changeDetails && onSubmit()
+                    setChangeDetails((prevState) => !prevState)
+                }}>{changeDetails ? 'done' : 'change'}</p>
+            </div>
+
+            <div className="profileCard">
+                <form>
+                    <input type="text" 
+                        id="name" 
+                        className={!changeDetails ? 'profileName' : 'profileNameActive'} 
+                        disabled={!changeDetails}
+                        value={name}
+                        onChange={onChange}
+                    />
+                    <input type="text" 
+                        id="email" 
+                        className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} 
+                        disabled={!changeDetails}
+                        value={email}
+                        onChange={onChange}
+                    />
+                </form>
+            </div>
+
+            <Link to='/create-listing' className='createListing'>
+                <img src={homeIcon} alt="home" />
+                <p>Sell or rent your home</p>
+                <img src={arrowRight} alt="arrow right" />
+            </Link>
+        </main>
+    </div>
+}
+
+export default Profile
