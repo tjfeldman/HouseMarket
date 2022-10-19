312e832c80ec2123ab44859ac18842b3d9672320
Feldman <625711@bah.com>
2022-10-19 16:06:44 -0400

    Added Listing Page and Contact page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Category from './pages/Category';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
@@ -24,6 +26,8 @@ function App() {
             <Route path='/profile' element={<Profile />} />
           </Route>
           <Route path = '/create-listing' element={<CreateListing />} />
+          <Route path = '/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path = '/contact/:landlordId' element={<Contact />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
